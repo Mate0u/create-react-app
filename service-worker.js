1ce17677fbb8cd18b06682478c2d038e67a6ecb8
Mateusz Ulanczyk <mateusz.ulanczyk@gmail.com>
2018-11-22 16:29:13 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/create-react-app/precache-manifest.dac5528b359e578be76d7d64b13340ce.js"
+  "/create-react-app/precache-manifest.fa26fd83b6b54a686e7d210cd872ff2d.js"
 );
 
 workbox.clientsClaim();
