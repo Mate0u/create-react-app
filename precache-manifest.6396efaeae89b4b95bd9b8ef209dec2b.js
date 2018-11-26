facdbf531206e408208adac4b39be4f42bec8c90
Mateusz Ulanczyk <mateusz.ulanczyk@gmail.com>
2018-11-26 15:19:33 +0100

    Updates

---
+++ precache-manifest.6396efaeae89b4b95bd9b8ef209dec2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d626e9e8f57d217751f",
+    "url": "/create-react-app/static/js/runtime~main.1d626e9e.js"
+  },
+  {
+    "revision": "6a4e2e79c1b74c73e13d",
+    "url": "/create-react-app/static/js/main.6a4e2e79.chunk.js"
+  },
+  {
+    "revision": "bbb91c8321977519c7eb",
+    "url": "/create-react-app/static/js/1.bbb91c83.chunk.js"
+  },
+  {
+    "revision": "6a4e2e79c1b74c73e13d",
+    "url": "/create-react-app/static/css/main.2d595cde.chunk.css"
+  },
+  {
+    "revision": "255162c8e66f4204053da266e262c85a",
+    "url": "/create-react-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/create-react-app/precache-manifest.fa26fd83b6b54a686e7d210cd872ff2d.js"
+  "/create-react-app/precache-manifest.6396efaeae89b4b95bd9b8ef209dec2b.js"
 );
 
 workbox.clientsClaim();
