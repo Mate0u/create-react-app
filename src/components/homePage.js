3c9f9a7f002a3a49a187362b6bd8dc26cf86d0a3
Mateusz Ulanczyk <Mateusz.ulanczyk@gmail.com>
2019-07-31 20:51:42 +0200

    New brunch for Material created

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+import AppBar from '@material-ui/core/AppBar';
 
 const Home = () => {
-  return (
-    <div className="container">
-      <h1>Home!!</h1>
-    </div>
-  );
+return (
+
+<div>
+ <AppBar color="primary" position="static">
+ <h1>My header</h1>
+ </AppBar>
+</div>
+
+);
 }
 
 export default Home;
