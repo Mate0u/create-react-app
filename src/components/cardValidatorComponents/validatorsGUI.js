cdae14d1042cabcd00af780262223ab44987fc73
Mateusz Ulanczyk <mateusz.ulanczyk@gmail.com>
2019-07-31 13:21:24 +0200

    Finished validator for check sum using luhn algoritm

--- src/components/cardValidatorComponents/validatorsGUI.js
+++ src/components/cardValidatorComponents/validatorsGUI.js
@@ -1,10 +1,14 @@
 import React from "react";
+const luhn = require("luhn");
 
 class validatorsGuiButtons extends React.Component{
 
 constructor (props) {
 super (props);
 this.state = {
+ lengthOfCardNumber: 16,
+ checkSum: luhn.validate(this.props.actualInputValue),
+ informationCheckSum: null,
  informationCardNumberLenght: null,
  infromationAboutCardNumberLenghtValidator: null
 };
@@ -16,18 +20,31 @@ if(this.props.actualInputValue === null){
 } else {
  this.setState({infromationAboutCardNumberLenghtValidator: ""});
  this.MetodValidationLenght();
+ this.MetodValidationCheckSum();
 }
  this.setState(state => ({valueToOutput: state.actualInputValue }));
  this.setState(state => ({previousActualInputValue: state.actualInputValue}));
 }
 
-// //Checking if card number have 16 digits
+//Checking if card number have 16 digits
 MetodValidationLenght(){
-if(this.props.actualInputValue.toString().length === 16){
+if(this.props.actualInputValue.toString().length === this.state.lengthOfCardNumber){
  this.setState({informationCardNumberLenght: "Card Number has a good length"});
 } else {
  this.setState({informationCardNumberLenght: "Card Number has not a good length"});
 }
+}
+
+//algoritm luhn validate a check sum of vard number
+//TODO: on first validate checksum is showing false
+MetodValidationCheckSum(){
+this.setState({checkSum: luhn.validate(this.props.actualInputValue)}, () => {
+ if(this.state.checkSum){
+  this.setState({informationCheckSum: "Card checksum is true"});
+ } else {
+  this.setState({informationCheckSum: "Card checksum is false"});
+ }
+});
 }
 
 render(){
@@ -37,6 +54,7 @@ return(
  <button onClick={this.onButtonClickSet.bind(this)}>Validate</button>
  <output>{this.state.infromationAboutCardNumberLenghtValidator}</output>
  <div><output>{this.state.informationCardNumberLenght}</output></div>
+ <div><output>{this.state.informationCheckSum}</output></div>
  </div>
 </div>
 )
